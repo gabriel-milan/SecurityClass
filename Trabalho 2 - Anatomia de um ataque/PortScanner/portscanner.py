67b2bc7253c31f32b359d0e4e5a93212918aea4b
Brenno Rodrigues de Carvalho da Silva <brennorcs@poli.ufrj.br>
2019-04-25 21:22:19 -0300

    Update portscanner.py
    Updated to python 3 and to scan the firts 1024 ports.

--- Trabalho 2 - Anatomia de um ataque/PortScanner/portscanner.py
+++ Trabalho 2 - Anatomia de um ataque/PortScanner/portscanner.py
@@ -3,21 +3,20 @@ import socket
 import subprocess
 import sys
 
-# Clear the screen
-subprocess.call('clear', shell=True)
-
 # Ask for input
-remoteServerIP  = raw_input("Enter IP: ")
+#print("Enter IP:")
+remoteServerIP = input("Enter IP:" )
+  
 
 # Print a nice banner with information on which host we are about to scan
-print "-" * 60
-print "Please wait, scanning remote host", remoteServerIP
-print "-" * 60
+print("-" * 60)
+print("Please wait, scanning remote host", remoteServerIP)
+print("-" * 60)
 
 # We also put in some error handling for catching errors
 
 try:
-    for port in range(500,501):  
+    for port in range(1,1025):  
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.settimeout(2)
@@ -26,18 +25,18 @@ try:
             s.send(b'Respond True')
             result = s.recv(1024)
             if result:
-                print "Port {}: Result: {}".format(port, result)
+                print("Port {}: Result: {}".format(port, result))
             else:
-                print "Port {}: No response.".format(port)
+                print("Port {}: No response.".format(port))
             s.close()   
         except socket.error:
-            print "Couldn't connect to port {}".format(port)
+            print( "Couldn't connect to port {}".format(port))
             pass
 
 except KeyboardInterrupt:
-    print "You pressed Ctrl+C"
+    print("You pressed Ctrl+C")
     sys.exit()
 
 except socket.gaierror:
-    print 'Hostname could not be resolved. Exiting'
+    print ('Hostname could not be resolved. Exiting')
     sys.exit()
